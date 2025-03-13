af3d5fcca9949fc02e489a135d2f8d03780e1b1b
sergey radionov <rsatom@gmail.com>
2025-03-13 13:00:26 +0700

    www: added stub image
    to show when there is no streamers configured

--- www/src/App.vue
+++ www/src/App.vue
@@ -6,44 +6,64 @@ const streamers = useStreamersStore()
 </script>
 
 <template>
-  <Card class="card" v-for="streamer of streamers.streamers" :key="streamer.id">
-    <template #header>
-      <div class="card-header-container">
-        <Player :uri="streamer.sourceUrl"/>
-      </div>
-    </template>
-    <template #title>
-      <span
-        class="title-subtitle"
-        :title="streamer.sourceUrl"
-      >
-        {{ streamer.sourceUrl }}
-      </span>
-    </template>
-    <template #subtitle>
-      <span
-        class="title-subtitle"
-        :title="streamer.description"
-      >
-        {{ streamer.description }}
-      </span>
-    </template>
-    <template #footer>
-      <div class="card-footer">
-        <Button
-          :icon="streamer.enabled ? 'pi pi-stop-circle' : 'pi pi-play-circle'"
-          :loading="streamer.sendingUpdate"
-          variant="text"
-          rounded
-          size="large"
-          :severity="streamer.enabled ? 'warn' : 'success'"
-          @click="streamers.toggleStreaming(streamer.id)"/>
-      </div>
-    </template>
-  </Card>
+  <div id="streamers" v-if="streamers.streamers.length != 0">
+    <Card class="card" v-for="streamer of streamers.streamers" :key="streamer.id">
+      <template #header>
+        <div class="card-header-container">
+          <Player :uri="streamer.sourceUrl"/>
+        </div>
+      </template>
+      <template #title>
+        <span
+          class="title-subtitle"
+          :title="streamer.sourceUrl"
+        >
+          {{ streamer.sourceUrl }}
+        </span>
+      </template>
+      <template #subtitle>
+        <span
+          class="title-subtitle"
+          :title="streamer.description"
+        >
+          {{ streamer.description }}
+        </span>
+      </template>
+      <template #footer>
+        <div class="card-footer">
+          <Button
+            :icon="streamer.enabled ? 'pi pi-stop-circle' : 'pi pi-play-circle'"
+            :loading="streamer.sendingUpdate"
+            variant="text"
+            rounded
+            size="large"
+            :severity="streamer.enabled ? 'warn' : 'success'"
+            @click="streamers.toggleStreaming(streamer.id)"/>
+        </div>
+      </template>
+    </Card>
+  </div>
+  <i v-else class="pi pi-ban stub"/>
 </template>
 
 <style scoped>
+  #streamers {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: var(--cards-gap);
+  }
+
+  /* min-width: calc(var(--card-width) * var(--cards-in-row) + (var(--cards-in-row) + 1) * var(--cards-gap))*/
+  @media (min-width: 79rem) {
+    #streamers {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-auto-flow: row;
+      gap: var(--cards-gap);
+    }
+  }
+
   .card {
     width: var(--card-width);
     overflow: hidden;
@@ -64,5 +84,14 @@ const streamers = useStreamersStore()
 
   .card-footer {
     text-align: center;
+  }
+
+  .stub {
+    font-size: calc(var(--card-width) / 2);
+    text-align: center;
+    color: var(--p-surface-300);
+    display: block;
+    line-height: 100vh;
+    margin: auto;
   }
 </style>
